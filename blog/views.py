cd1e91cadb8b8259058bf497a93f832f84b35d41
Vladimir Filippov <sergeiych@gmail.com>
2019-03-27 17:29:24 +0300

    after 5 tut (amend)

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,29 @@
 from django.shortcuts import render
-# Create your views here.
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post1',
-        'content': 'First post content',
-        'date_posted' : 'August 27, 2018',
-    },
-    {
-        'author': 'Jon Doe',
-        'title': 'Blog Post2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018',
-    },
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
+
+
+# posts = [
+#     {
+#         'author': 'CoreyMS',
+#         'title': 'Blog Post1',
+#         'content': 'First post content',
+#         'date_posted' : 'August 27, 2018',
+#     },
+#     {
+#         'author': 'Jon Doe',
+#         'title': 'Blog Post2',
+#         'content': 'Second post content',
+#         'date_posted': 'August 28, 2018',
+#     },
+# ]
